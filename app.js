053399e37d48a1036bd95be5f1ca179a6175766f
Priya <rathipriya1001@gmail.com>
2020-07-09 14:19:03 +0530

    wwerqw

--- app.js
+++ app.js
@@ -1,56 +1,83 @@
 (function () {
-	'use strict';
+  angular.module('ShoppingListCheckOff',[])
+         .controller('ToBuyController', ToBuyController)
+         .controller('AlreadyBoughtController', AlreadyBoughtController)
+         .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-	angular.module('LunchCheck',[]).controller('LunchCheckController', LunchCheckController);
+          //To buy list
+         	ToBuyController.$inject =['ShoppingListCheckOffService'];
+         	function ToBuyController (ShoppingListCheckOffService)
+          {
+         		var buy = this;
+         		buy.items = ShoppingListCheckOffService.toBuyItems();
+         		buy.removeItem = function(itemIndex){
+         			ShoppingListCheckOffService.bought(itemIndex);
+         		};
+         	}
 
-	LunchCheckController.$inject =['$scope'];
+          //Already bought list
+        	AlreadyBoughtController.$inject =['ShoppingListCheckOffService'];
+        	function AlreadyBoughtController (ShoppingListCheckOffService)
+          {
+        		var bought = this;
+        		bought.items = ShoppingListCheckOffService.boughtItems();
+        	}
 
-	function LunchCheckController ($scope){
-		$scope.list = '';
-		$scope.msg = '';
-		$scope.fontStyle = {};
-		$scope.boxStyle = {};
+          //Shopping list service
+        	function ShoppingListCheckOffService()
+          {
+        		var service = this;
+            var toBuyItems =
+            [
+              {name: "Cookies",
+               quantity: 10
+              },
+              {name: "Chocolate",
+               quantity: 3
+              },
+              {name: "Notebook",
+               quantity: 5
+              },
+              {name: "Pens",
+               quantity: 12
+              },
+              {name: "iPhones 7",
+               quantity: 4
+              },
+              {name: "MK bag",
+               quantity: 1
+              },
+              {name: "Laptop",
+               quantity: 4
+              },
+              {name: "Potato",
+               quantity: 8
+              },
+              {name: "Rings",
+               quantity: 2
+              },
+              {name: "Tissues",
+               quantity: 90
+              },
+              {name: "Glasses",
+               quantity: 17
+              }
+            ];
 
-		$scope.checkItems = function () {
-			console.log ('check Items');
-			console.log ($scope.list);
-			if($scope.list !== '') {
-				var arr = $scope.list.split(',');
-				var count =0;
-				for(var i =0; i<arr.length; i++){
-					if(arr[i].trim() !=='')
-						count++;
-				}
-				if(count > 0){
-					if(count <=3){
-						$scope.msg = 'Enjoy!';
-					}
-					else if(count > 3){
-						$scope.msg = 'Too much!'
-					}
-					$scope.fontStyle = {
-						"color":"green"
-					};
-					$scope.boxStyle = {
-						"border-color":"green"
-					};
-				}
-				else {
-					enterItems();
-				}
-			}
-			else {
-				enterItems();
-			}
-		}
-		var enterItems = function() {
-			$scope.msg = 'Please enter data first';
-				$scope.fontStyle = {
-					"color":"red"
-				};
-				$scope.boxStyle = {
-					"border-color":"red"
-				};
-		};
-	}
+        		var boughtItems = [];
+
+          	service.bought = function(itemIndex) {
+          			boughtItems.push(toBuyItems[itemIndex]);
+          			toBuyItems.splice(itemIndex, 1);
+          	};
+
+          	service.boughtItems = function() {
+          			return boughtItems;
+          	};
+
+          	service.toBuyItems = function() {
+          			return toBuyItems;
+          	};
+          }
+
 })();
